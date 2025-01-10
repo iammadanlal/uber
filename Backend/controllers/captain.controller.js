f92909880e5ca2396cba1d74fe883bdd37446c74
Madan Lal <iammadanlalmali@gmail.com>
2025-01-10 19:38:25 +0530

    feat: captain login, get profile api done 🚀

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -21,8 +21,40 @@ module.exports.registerCaptain = async (req, res) => {
 		password: hashedPassword,
 		vehicle,
 	});
+	const { password: _, ...captainWithoutPassword } = await captain.toObject();
+	const token = captain.generateAuthToken();
+	res.cookie("token", token);
+	return res.status(201).json({ token, captain: captainWithoutPassword });
+};
+
+module.exports.loginCaptain = async (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	const { email, password } = req.body;
+
+	const captain = await captainModel.findOne({ email }).select("+password");
+
+	if (!captain) {
+		return res.status(401).json({ message: "Invalid email or password" });
+	}
+
+	const isMatch = await captain.comparePassword(password);
+
+	if (!isMatch) {
+		return res.status(400).json({ message: "Invalid email or password" });
+	}
+
+	const { password: _, ...captainWithoutPassword } = await captain.toObject();
 
 	const token = captain.generateAuthToken();
 	res.cookie("token", token);
-	return res.status(201).json({ token, captain });
+
+	res.status(200).json({ token, captain: captainWithoutPassword });
+};
+
+module.exports.getCaptainProfile = async (req, res) => {
+	return res.status(200).json({ captain: req.captain });
 };
--- Backend/routes/captain.route.js
+++ Backend/routes/captain.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const captainController = require("../controllers/captain.controller");
 const { body } = require("express-validator");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post(
 	"/register",
@@ -27,6 +28,23 @@ router.post(
 			.withMessage("Invalid vehicle type"),
 	],
 	captainController.registerCaptain
+);
+
+router.post(
+	"/login",
+	[
+		body("email").isEmail().withMessage("Please enter a valid email"),
+		body("password")
+			.isLength({ min: 6 })
+			.withMessage("Password must be at least 6 characters long"),
+	],
+	captainController.loginCaptain
+);
+
+router.get(
+	"/profile",
+	authMiddleware.authCaptain,
+	captainController.getCaptainProfile
 );
 
 module.exports = router;
